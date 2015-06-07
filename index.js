c0934a9611235baa5aef2bc00b534c5d46a59a1a
Shikkic <Shikkic@gmail.com>
2015-06-07 03:27:12 -0400

    more workkk

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var request = require('request');
+var async = require('async');
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static('./public'));
 
 var morgan = require('morgan');
 
@@ -25,11 +28,25 @@ app.get('/git', function(req, res) {
         if(!error) {
             var data = JSON.parse(body);
             console.log("made it");
-            res.send(data);
+            var count = 0;
+            var found = false;
+
+            while(found != true && count < data.length) {
+                console.log(data[count].type);
+                if(data[count].type == "PushEvent") {
+                    found = true;
+                    break;
+                }
+                count++;
+            }
+            
+            res.send(data[count]);
         }
     });
 });
 
-app.listen(3000, function() {
-    console.log("listening on port: " + 3000);
+app.listen(app.get('port'), function() {
+    console.log("listening on port: " + app.get('port'));
 });
+
+
