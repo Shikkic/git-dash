497d2a1d5b4e99f4eec4c9b89e012e4a6306312a
Shikkic <Shikkic@gmail.com>
2015-07-07 01:26:15 -0400

    updates

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-
-   var app = {};
-   app.Git = Backbone.Model.extend({
+   GitCard = Backbone.Model.extend({
       defaults: {
         imgUrl: '',
         repoName: '',
@@ -15,7 +13,7 @@ $(document).ready(function() {
 
       tagName: 'li',
 
-      template: _.template("<li  class='col s4 truncate card-panel hoverable animated zoomIn git-cards'"+"><a href='http://github.com/<%= user %>'><img id="+"profile"+" src=<%= imgUrl %>/><p id='name'><%= user %></a></p><p id='date'><%= date %></p><a href='https://github.com/<%= repoName %>'><p id="+"repo"+"><%= repoName %></a><p id=msg>Commit Msg: <%= commitMsg %></p></li>"),
+      template: _.template("<li  class='col s4 truncate card-panel hoverable animated zoomIn git-cards'"+"><a href='http://github.com/<%= user %>'><img class="+"profile"+" src=<%= imgUrl %>/><p class='name'><%= user %></a></p><p class='date'><%= date %></p><a href='https://github.com/<%= repoName %>'><p class="+"repo"+"><%= repoName %></a><p class=msg>Commit Msg: <%= commitMsg %></p></li>"),
       
       initialize: function(){
         this.render();
@@ -26,6 +24,14 @@ $(document).ready(function() {
         return this;
       }
     }); 
+
+    var ProfileCollection = Backbone.Collection.extend({
+        comparator: function(a) {
+            return a.get('date');
+        }
+    });
+
+    var profileCollection = new ProfileCollection();
 
     var paramInfo = '';
     var objData = '';
@@ -44,15 +50,20 @@ $(document).ready(function() {
                 $('#spinner').hide();
                 console.log(data);
                 for(var i in data) {
-                    var gitapp = new app.Git({
-                        imgUrl: data[i].actor.avatar_url, 
-                        user: data[i].actor.login, 
-                        repoName: data[i].repo.name, 
-                        commitMsg: data[i].payload.commits[0].message,
-                        date: new Date(data[i].created_at)
-                    }); 
-                    var view = new ProfileView({model: gitapp});
+                    if(data[i]) {
+                        var gitCard = new GitCard({
+                            imgUrl: data[i].actor.avatar_url, 
+                            user: data[i].actor.login, 
+                            repoName: data[i].repo.name, 
+                            commitMsg: data[i].payload.commits[0].message,
+                            date: "Last pushed "+moment(data[i].created_at).fromNow()
+                        });
+                    
+                    profileCollection.add(gitCard);
+                    var view = new ProfileView({model: gitCard});
+                    }
                 }
+                console.log(profileCollection);
             })
             .fail(function() {
                 alert("ajax failed to fetch data");
