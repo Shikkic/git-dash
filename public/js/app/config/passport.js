68b585b6a60c388e177d54b653859814e435ee90
Shikkic <Shikkic@gmail.com>
2015-07-15 01:35:58 -0400

    update

--- public/js/app/config/passport.js
+++ public/js/app/config/passport.js
@@ -39,7 +39,33 @@ module.exports = function(passport) {
       },
       function(accessToken, refreshToken, profile, done) {
         User.findOrCreate({ githubId: profile.id }, function (err, user) {
-           return done(err, user);         
+        if (err) {
+            return done(err);
+        }
+        // if the user is found, then log them in
+        if (user) {
+            return done(null, user); // user found, return that user
+        } else {
+            // if there is no user found with that facebook id, create them
+            var newUser            = new User();
+
+            // set all of the facebook information in our user model
+            newUser.github.id    = profile.id; // set the users facebook id                   
+            newUser.github.token = token; // we will save the token that facebook provides to the user                    
+            newUser.github.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
+            newUser.github.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+
+            // save our user to the database
+            newUser.save(function(err) {
+                if (err)
+                    throw err;
+
+                // if successful, return the new user
+                return done(null, newUser);
+            });
+        }
+ 
+        
         });
       }
     ));
