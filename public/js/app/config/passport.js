415de1fa44ac04cf476b68de25932800736676a2
Shikkic <Shikkic@gmail.com>
2015-07-15 00:11:26 -0400

    update

--- public/js/app/config/passport.js
+++ public/js/app/config/passport.js
@@ -35,7 +35,7 @@ module.exports = function(passport) {
     passport.use(new GithubStrategy({
         clientID: GITHUB_CLIENT_ID,
         clientSecret: GITHUB_CLIENT_SECRET,
-        callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+        callbackURL: "http://gitdash.me/auth/github/callback"
       },
       function(accessToken, refreshToken, profile, done) {
         User.findOrCreate({ githubId: profile.id }, function (err, user) {
