c64aa256e78ead831db04931e60c061a5e8cd39b
Shikkic <Shikkic@gmail.com>
2015-09-27 14:53:08 -0400

    changed the gulpfile structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,28 @@ var gulp = require('gulp'),
     exec = require('child_process').exec,
     jshint = require('gulp-jshint');
 
+gulp.task('default', ['run', 'watch']);
+
 gulp.task('run', function(cb) {
-    exec('mongod ', function (err, stdout, stderr) {
+    exec('mongod', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         cb(err);
     });
-    exec('node server/index.js', function (err, stdout, stderr) {
+    exec('node ./server/index.js', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         cb(err);
     });
-    // Launch Mongo incombination for deploy
 });
 
-gulp.task('default', ['watch']);
+gulp.task('ngrok', function(cb) {
+    exec('ngrok http 3000', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+});
 
 gulp.task('jshint', function() {
     return gulp.src('./public/js/app.js')
