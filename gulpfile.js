a84194706b7d0cec88102ed21f0759bb221c457e
Shikkic <Shikkic@gmail.com>
2015-09-26 20:11:00 -0400

    updating/creating gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,30 @@
-var gulp = require('gulp');
-var exec = require('child_process').exec;
+var gulp = require('gulp'),
+    exec = require('child_process').exec,
+    jshint = require('gulp-jshint');
 
 gulp.task('run', function(cb) {
+    exec('mongod ', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
     exec('node server/index.js', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         cb(err);
     });
     // Launch Mongo incombination for deploy
-    /*exec('mongod --dbpath ./data', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });*/
-})
+});
+
+gulp.task('default', ['watch']);
+
+gulp.task('jshint', function() {
+    return gulp.src('./public/js/app.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {verbose: true})); 
+
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./public/js/app.js', ['jshint']);
+});
