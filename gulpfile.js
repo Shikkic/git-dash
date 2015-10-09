17a01b5130c38cc98691a2cbf8d07e04d354af10
Dan Cadden <shikkic@gmail.com>
2015-10-09 01:06:31 -0400

    removed logging from gulpfile, buffer overload, for now

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,15 @@ gulp.task('default', ['run', 'watch']);
 
 gulp.task('run', function(cb) {
     exec('mongod', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
         cb(err);
     });
     exec('node ./server/index.js', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
         cb(err);
     });
 });
 
 gulp.task('ngrok', function(cb) {
     exec('ngrok http 3000', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
         cb(err);
     });
 });
