a00fb98b1ce775ef998a177c26b2b893b879d55c
Dan Cadden <shikkic@gmail.com>
2016-02-14 22:22:52 -0500

    implemented basic profile hide and show functionality

--- public/js/views/my-profile-view.js
+++ public/js/views/my-profile-view.js
@@ -12,7 +12,25 @@ define([
         },
 
         toggleProfile: function() {
-            console.log("step one");
+            if ($('#my-profile').hasClass('show-my-profile')) {
+                this.hideProfile();
+            } else {
+                this.showProfile();
+            }
+        },
+
+        showProfile: function() {
+            $('#my-profile').removeClass("hide-my-profile");
+            $('#container').removeClass('hide-profile');
+            $('#container').addClass('show-profile');
+            $('#my-profile').addClass("show-my-profile");
+        },
+
+        hideProfile: function() {
+            $('#my-profile').removeClass("show-my-profile");
+            $('#container').removeClass('show-profile');
+            $('#my-profile').addClass('hide-my-profile');
+            $('#container').addClass('hide-profile');
         }
 
     });
